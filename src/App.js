827069fc5233c94ccea87bcbbf93b290f8971eb6
Nakka553 <nakkaramesh.nr@gmail.com>
2023-02-24 14:32:09 +0530

    22-02-2023

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Content from './components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Content/>
+       
+    </div>
+  );
+}
+
+export default App;
